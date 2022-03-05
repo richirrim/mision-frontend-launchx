13001e04f5eb231ce26471639bf68c2605818fde
richirrim <ricardoortega.dev@gmail.com>
2022-03-05 16:32:26 -0600

    Se agrego la seccón de sabores.

--- Practica2-pasteleria/pasteleria-double-l/script.js
+++ Practica2-pasteleria/pasteleria-double-l/script.js
@@ -1,1 +1,28 @@
-pt
+const flavorsEl = document.getElementById('js-flavors')
+const textEl = document.getElementById('js-selected-flovers')
+const checkboxListEl = Array.from(document.querySelectorAll('.flavors input[type=checkbox]'))
+let contador = 0
+
+flavorsEl.addEventListener('change',  function(e) {
+    if (e.target.checked) {
+        contador++
+        textEl.textContent = `${contador}/2`
+    } else {
+        contador--
+        textEl.textContent = `${contador}/2`
+    }
+    
+    if (contador == 2) {
+        for (check of checkboxListEl) {
+            if (check.checked == false) {
+                check.setAttribute('disabled', '')
+            }
+        }
+    } else {
+        for (check of checkboxListEl) {
+            if (check.checked == false) {
+                check.removeAttribute('disabled')
+            }
+        }
+    }
+})
