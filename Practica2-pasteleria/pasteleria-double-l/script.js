9b04658a19b71fa6e18bc30a7333f327a2ec2fc3
richirrim <ricardoortega.dev@gmail.com>
2022-03-05 20:16:24 -0600

    Se creo la sección de toppings.

--- Practica2-pasteleria/pasteleria-double-l/script.js
+++ Practica2-pasteleria/pasteleria-double-l/script.js
@@ -3,6 +3,11 @@ const textEl = document.getElementById('js-selected-flovers')
 const checkboxListEl = Array.from(document.querySelectorAll('.flavors input[type=checkbox]'))
 let contador = 0
 
+const toppingsEl = document.getElementById('js-toppings')
+const toppingTextEl = document.getElementById('js-selected-toppings')
+const toppingCheckboxListEl = Array.from(document.querySelectorAll('.toppings input[type=checkbox]'))
+let contadorTopping = 0
+
 flavorsEl.addEventListener('change',  function(e) {
     if (e.target.checked) {
         contador++
@@ -20,6 +25,30 @@ flavorsEl.addEventListener('change',  function(e) {
         }
     } else {
         for (check of checkboxListEl) {
+            if (check.checked == false) {
+                check.removeAttribute('disabled')
+            }
+        }
+    }
+})
+
+toppingsEl.addEventListener('change',  function(e) {
+    if (e.target.checked) {
+        contadorTopping++
+        toppingTextEl.textContent = `${contadorTopping}/4`
+    } else {
+        contadorTopping--
+        toppingTextEl.textContent = `${contadorTopping}/4`
+    }
+    
+    if (contadorTopping == 4) {
+        for (check of toppingCheckboxListEl) {
+            if (check.checked == false) {
+                check.setAttribute('disabled', '')
+            }
+        }
+    } else {
+        for (check of toppingCheckboxListEl) {
             if (check.checked == false) {
                 check.removeAttribute('disabled')
             }
